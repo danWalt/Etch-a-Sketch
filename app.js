d7eaff48f0d1e9dd1216eaeec278373ec86fb841
daniel.walters <daniel.walters2110@gmail.com>
2021-01-23 13:45:14 +0200

    changing colors and resetting board works

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const container = document.getElementById("container");
+(() => {
 
+const container = document.getElementById("container");
+const h1 = document.createElement('h1')
+h1.textContent = 'Etch-a-Sketch'
+h1.className = 'headline'
+container.parentElement.insertBefore(h1,container)
 
 function makeRows(rows, cols){
     container.style.setProperty('--grid-rows', rows);
@@ -10,6 +15,7 @@ function makeRows(rows, cols){
         container.appendChild(cell).className = "grid-item";      
     }
 }
+makeRows(16, 16)
 
 
 
@@ -17,6 +23,32 @@ function insertAfter(newNode, referenceNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
+const gridItems = document.querySelectorAll(".grid-item")
+window.addEventListener('mouseover', () => {
+gridItems.forEach(block => {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    block.addEventListener('mouseover',() => {
+        block.style.backgroundColor = "#" + randomColor;
+    })
+})
+
+})
+const resetDiv = document.createElement('div')
+resetDiv.className = 'reset-button-div'
+
+const resetButton = document.createElement('button')
+resetButton.textContent = 'Click Here to Reset'
+resetButton.className = 'reset-button'
+
+insertAfter(resetDiv, container)
+
+resetDiv.appendChild(resetButton)
 
 
-makeRows(16, 16)
+resetButton.addEventListener('click', () => {
+    gridItems.forEach(block => {
+        block.style.backgroundColor = 'white';
+    })
+})
+
+})()
