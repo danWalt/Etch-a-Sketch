815b768333b4b4ab6627a8851cb1fed883ca02e9
daniel.walters <daniel.walters2110@gmail.com>
2021-01-24 21:25:12 +0200

    update

--- app.js
+++ app.js
@@ -5,34 +5,8 @@ const h1 = document.createElement('h1')
 h1.textContent = 'Etch-a-Sketch'
 h1.className = 'headline'
 container.parentElement.insertBefore(h1,container)
+makeGrid(16)
 
-function makeRows(rows, cols){
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (let index = 0; index < rows*cols; index++) {
-        let cell = document.createElement('div')
-        cell.innerText = ''
-        container.appendChild(cell).className = "grid-item";      
-    }
-}
-makeRows(16, 16)
-
-
-
-function insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
-
-let gridItems = document.querySelectorAll(".grid-item")
-window.addEventListener('mouseover', () => {
-gridItems.forEach(block => {
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    block.addEventListener('mouseover',() => {
-        block.style.backgroundColor = "#" + randomColor;
-    })
-})
-
-})
 const resetDiv = document.createElement('div')
 resetDiv.className = 'reset-button-div'
 
@@ -42,7 +16,16 @@ resetButton.className = 'reset-button'
 resetDiv.appendChild(resetButton)
 
 document.body.insertBefore(resetDiv, document.body.firstChild);
+let gridItems = document.querySelectorAll(".grid-item")
 
+window.addEventListener('mouseover', () => {
+    gridItems.forEach(block => {
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        block.addEventListener('mouseover',() => {
+            block.style.backgroundColor = "#" + randomColor;
+            })
+        })
+    })
 
 
 resetButton.addEventListener('click', () => {
@@ -53,15 +36,21 @@ resetButton.addEventListener('click', () => {
     gridItems.forEach(block => {
         block.remove()
     })
-    makeRows(gridSize, gridSize)
+    
+
+    makeGrid(gridSize)
     gridItems = document.querySelectorAll(".grid-item")
-
-
-
+    
 })
-
-
-
-
-
+    
+function makeGrid(size){
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    for (let index = 0; index < size*size; index++) {
+        let block = document.createElement('div')
+        let blockSize = (576/size) - 1
+        block.style.width = blockSize +'px';
+        block.style.height = blockSize +'px';
+        container.appendChild(block).className = "grid-item";      
+    }
+}
 })()
